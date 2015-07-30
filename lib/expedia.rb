1a0cf5b6c68e5672ec1a23a7f42390019a3ea4fc
Kelly Mahan <kmahan@kmahan.com>
2015-07-30 10:29:09 -0500

    Ran into an issue where the signature wasn't required since I used IP whitelisting. Talked with a tech at expedia and they mentioned a change on their end that if a signature is provided the ip whitelisting is ignored. This change will allow users to disable passing along the signature.

--- lib/expedia.rb
+++ lib/expedia.rb
@@ -23,11 +23,12 @@ module Expedia
   class << self
 
     attr_accessor :cid, :api_key, :shared_secret, :format, :locale,
-      :currency_code, :minor_rev, :timeout, :open_timeout
+      :currency_code, :minor_rev, :timeout, :open_timeout, :use_signature
 
     # Default way to setup Expedia. Run generator to create
     # a fresh initializer with all configuration values.
     def setup
+      Expedia.use_signature = true #default
       yield self
     end
 
